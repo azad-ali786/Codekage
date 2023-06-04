4cc024ced3dd57ed788e03085296335219fd8638
azad-ali786 <azad_ug@cse.nits.ac.in>
2023-06-05 00:25:47 +0530

    Fixed issued related Eslint and next/babel

--- components/CodeEditor/index.tsx
+++ components/CodeEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import _debounce from "lodash/debounce";
 
